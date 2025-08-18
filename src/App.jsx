3725152e6f2d1b988213e5050367c9b8c7e5b6b8
Devansh Deep Gupta <devanshdeepgupta@Devanshs-MacBook-Air.local>
2025-08-18 21:44:38 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
